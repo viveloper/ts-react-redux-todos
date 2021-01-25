9008c6990465d6b3167ee6e228b89ddfa385a2c3
viveloper <ucsik1234@gmail.com>
2021-01-26 07:24:35 +0900

    implementing the todo app

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './modules';
+
+const store = createStore(rootReducer);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
